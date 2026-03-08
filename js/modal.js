7d7f45b9be1cb39003036fb410e3e6c41f854782
Марина Махалова <aecktan-himera@mail.ru>
2026-03-08 11:55:21 +0300

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,20 @@
-const modal = document.querySelector(".search-model");
-const modalBtn = document.querySelector(".icon_search");
-const modalClose = modal.querySelector(".search-close-switch");
-const searchInput = modal.querySelector("#search-input");
+const modal = () => {
+  const modal = document.querySelector(".search-model");
+  const modalBtn = document.querySelector(".icon_search");
+  const modalClose = modal.querySelector(".search-close-switch");
+  const searchInput = modal.querySelector("#search-input");
 
-modalBtn.addEventListener('click', () => {
+  modalBtn.addEventListener("click", () => {
     modal.style.display = "block";
-})
+  });
 
-modalClose.addEventListener('click', () => {
+  modalClose.addEventListener("click", () => {
     modal.style.display = "none";
-})
+  });
 
-searchInput.addEventListener('input', () => {
+  searchInput.addEventListener("input", () => {
     console.log(searchInput.value);
-})
+  });
+};
+
+modal();
